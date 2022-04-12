ded13855a08170dafd788f2391f1a30748acff35
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-04-12 10:15:38 -0500

    refacrotring code

--- game.js
+++ game.js
@@ -11,20 +11,44 @@ const computerPlay = () => {
     return "Scissors";
   }
 };
-console.log(computerPlay());
+
+const playerChoice = () => {
+  let playerChoice = prompt("Choose rock, paper, or scissors");
+  playerChoice = playerChoice.toLowerCase();
+  if (
+    playerChoice === "rock" ||
+    playerChoice === "paper" ||
+    playerChoice === "scissors"
+  ) {
+    return playerChoice;
+  } else {
+    alert("Try a valid choice");
+  }
+};
+
+let playerSelection = playerChoice();
+let computerSelection = computerPlay();
 
 const gameRound = (playerSelection, computerSelection) => {
-  if (playerSelection == "Rock" && computerSelection == "Paper") {
+  console.log(playerSelection);
+  if (playerSelection == "rock" && computerSelection == "Paper") {
     return "You Lose!, Paper beats Rock";
-  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+  } else if (playerSelection == "paper" && computerSelection == "Scissors") {
     return "You Lose!, Scissors beats Paper";
-  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+  } else if (playerSelection == "scissors" && computerSelection == "Rock") {
     return "You Lose!, Rock beats Scissors";
-  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+  } else if (playerSelection == "rock" && computerSelection == "Scissors") {
     return "You Win!, Rock beats Scissors";
-  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+  } else if (playerSelection == "paper" && computerSelection == "Rock") {
     return "You Win!, Paper beats Rock";
-  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+  } else if (playerSelection == "scissors" && computerSelection == "Paper") {
     return "You Win!, Scissors beats Paper";
   }
+};
+const player = "rock";
+const comp = computerPlay();
+gameRound(playerSelection, computerSelection);
+
+const game = () => {
+  for (let i = 0; i <= 5; i++) {}
 };
